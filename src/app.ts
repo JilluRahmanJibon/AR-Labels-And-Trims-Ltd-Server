82fb277113f69882817cbaae673ec5971557fd20
JilluRahmanJibon <jillurahmanjibon@gmail.com>
2024-11-11 23:21:46 +0600

    try to fixed error

--- src/app.ts
+++ src/app.ts
@@ -4,32 +4,25 @@ import router from './app/routes'
 import globalErrorHandler from './app/middlewares/globalErrorHandler'
 import notFound from './app/middlewares/notFound'
 
-
-
 const app: Application = express()
 
-// parser 
+// parser
 app.use(express.json())
 
-app.use(
-  cors({
-    // origin: 'http://localhost:3000',
-    origin: 'https://ar-labels-and-trims-ltd.vercel.app',
-    credentials: true,
-  }),
-)
+app.use(cors())
+// {
+//     // origin: 'http://localhost:3000',
+//     origin: 'https://ar-labels-and-trims-ltd.vercel.app',
+//     credentials: true,
+//   }
 
-    
-
-// application routes 
+// application routes
 app.use('/api/v1', router)
 
-// global error handler 
+// global error handler
 app.use(globalErrorHandler)
-  
 
-// API NOT FOUND 
-app.use(notFound) 
-
+// API NOT FOUND
+app.use(notFound)
 
 export default app
